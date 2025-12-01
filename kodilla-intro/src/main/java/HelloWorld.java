432ac068a4c0bcb3fa9c6ee2a3aea7f79f1096d2
Amadeusz Kielesinski <ama4deus@gmail.com>
2025-12-01 01:27:51 +0100

    Version 1

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Amadeusz");
+        System.out.println(35);
+        System.out.println('K');
+    }
+}
+
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Jane";
+        String surname = "Doe";
+        int number = 7;
+        double lucky = 7.77;
+        boolean good = true;
+        char letter = 'A';
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(number);
+        System.out.println(lucky);
+        System.out.println(good);
+        System.out.println(letter);
+    }
+}
